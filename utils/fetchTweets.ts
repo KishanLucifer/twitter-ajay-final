28a9be6dc592c5e20da7a2455ad2c8af85d1a2b7
kishan <kishanrokk@gmail.com>
2023-10-30 10:33:12 +0530

    cheking

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,22 +1,10 @@
 import { Tweet } from "../typings";
 
 export const fetchTweets = async () => {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`,{cache: 'no-store'}
-    );
-    
-    
-    
-    if (!res.ok) {
-      throw new Error(`Failed to fetch tweets: HTTP status ${res.status}`);
-    }
-
-    const data = await res.json();
-    const tweets: Tweet[] = data.tweets;
-    return tweets;
-  } catch (error) {
-    console.error("Error fetching tweets:", error);
-    return []; // or handle the error in a way that makes sense for your application
-  }
+  const res = await fetch(
+    ` ${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets `
+  );
+  const data = await res.json();
+  const tweets: Tweet[] = data.tweets;
+  return tweets;
 };
-
