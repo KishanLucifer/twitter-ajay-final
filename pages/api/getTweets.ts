169de838a1b057cdbda97f2bd44ae8154ef74041
kishan <kishanrokk@gmail.com>
2023-10-30 10:06:46 +0530

    update for fresh tweets

--- pages/api/getTweets.ts
+++ pages/api/getTweets.ts
@@ -1,25 +1,39 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { groq } from "next-sanity";
+// import { Tweet } from "@/typings";
+// import { sanityClient } from "../../sanity";
+
+// const feedQuery = groq`
+//     *[_type == "tweet" && !blockTweet] {
+//         _id,
+//         ...
+//     } | order(_createdAt desc)`;
+
+// type Data = {
+//   tweets: Tweet[];
+// };
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   const tweets: Tweet[] = await sanityClient.fetch(feedQuery);
+//   console.log(tweets);
+//   res.status(200).json({ tweets });
+// }
+
+// utils/sanity.ts
 import { groq } from "next-sanity";
-import { Tweet } from "@/typings";
 import { sanityClient } from "../../sanity";
+import { Tweet } from "@/typings";
 
 const feedQuery = groq`
-    *[_type == "tweet" && !blockTweet] {
-        _id,
-        ...
-    } | order(_createdAt desc)`;
+  *[_type == "tweet" && !blockTweet] {
+    _id,
+    ...
+  } | order(_createdAt desc)`;
 
-type Data = {
-  tweets: Tweet[];
-};
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const tweets: Tweet[] = await sanityClient.fetch(feedQuery);
-  console.log(tweets);
-  res.status(200).json({ tweets });
+export async function fetchTweets(): Promise<Tweet[]> {
+  return sanityClient.fetch(feedQuery);
 }
-
