e4f77fd08d9432dfefa50a1574bb84e0b200420d
kishan <kishanrokk@gmail.com>
2023-10-31 11:59:28 +0530

    for fresh data render

--- pages/api/getTweets.ts
+++ pages/api/getTweets.ts
@@ -1,3 +1,28 @@
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { groq } from "next-sanity";
+// import { Tweet } from "@/typings";
+// import { sanityClient } from "../../sanity";
+
+// const feedQuery = groq`
+//     *[_type == "tweet" && !blockTweet] {
+//         _id,
+//         ...
+//     } | order(_createdAt desc)`;
+
+// type Data = {
+//   tweets: Tweet[];
+// };
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   const tweets: Tweet[] = await sanityClient.fetch(feedQuery);
+//   console.log(tweets);
+//   res.status(200).json({ tweets });
+// }
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
@@ -14,11 +39,23 @@ type Data = {
   tweets: Tweet[];
 };
 
+type ErrorResponse = {
+  error: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | ErrorResponse>
 ) {
-  const tweets: Tweet[] = await sanityClient.fetch(feedQuery);
-  console.log(tweets);
-  res.status(200).json({ tweets });
+  try {
+    const tweets: Tweet[] = await sanityClient.fetch(feedQuery);
+
+    // Use revalidate function to control caching behavior
+    res.setHeader('Cache-Control', 's-maxage=60'); // Adjust the caching duration as needed (e.g., 60 seconds)
+    
+    res.status(200).json({ tweets });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Error fetching data" } as ErrorResponse);
+  }
 }
