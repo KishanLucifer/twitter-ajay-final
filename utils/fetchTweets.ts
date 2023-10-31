299f133148436c51e15b429fa658c3f7e40a80f3
kishan <kishanrokk@gmail.com>
2023-10-31 12:35:46 +0530

    done

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,32 +1,11 @@
-// import { Tweet } from "../typings";
-
-// export const fetchTweets = async () => {
-//   const res = await fetch(
-//     ` ${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets `
-//   );
-//   const data = await res.json();
-//   const tweets: Tweet[] = data.tweets;
-//   return tweets;
-// };
-
 import { Tweet } from "../typings";
 
-
 export const fetchTweets = async () => {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`);
-    
-    if (!res.ok) {
-      throw new Error(`Failed to fetch data: ${res.statusText}`);
-    }
-
-    const data = await res.json();
-
-    
-    const tweets: Tweet[] = data.tweets;
-    return tweets;
-  } catch (error) {
-    console.error("Error fetching tweets:", error);
-    throw error; // You can re-throw the error or handle it as needed.
-  }
+  const res = await fetch(
+    ` ${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets `
+  );
+  const data = await res.json();
+  const tweets: Tweet[] = data.tweets;
+  return tweets;
 };
+
