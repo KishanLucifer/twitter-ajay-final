dca5c45fdb345088ab656e703af786894decc9bc
kishan <kishanrokk@gmail.com>
2023-10-31 12:28:29 +0530

    for fresh

--- pages/api/getTweets.ts
+++ pages/api/getTweets.ts
@@ -1,33 +1,9 @@
-// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import { groq } from "next-sanity";
-// import { Tweet } from "@/typings";
-// import { sanityClient } from "../../sanity";
-
-// const feedQuery = groq`
-//     *[_type == "tweet" && !blockTweet] {
-//         _id,
-//         ...
-//     } | order(_createdAt desc)`;
-
-// type Data = {
-//   tweets: Tweet[];
-// };
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   const tweets: Tweet[] = await sanityClient.fetch(feedQuery);
-//   console.log(tweets);
-//   res.status(200).json({ tweets });
-// }
-
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { Tweet } from "@/typings";
 import { sanityClient } from "../../sanity";
+import { revalidatePath } from 'next/cache'
 
 const feedQuery = groq`
     *[_type == "tweet" && !blockTweet] {
@@ -39,23 +15,12 @@ type Data = {
   tweets: Tweet[];
 };
 
-type ErrorResponse = {
-  error: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data | ErrorResponse>
+  res: NextApiResponse<Data>
 ) {
-  try {
-    const tweets: Tweet[] = await sanityClient.fetch(feedQuery);
-
-    // Use revalidate function to control caching behavior
-    res.setHeader('Cache-Control', 's-maxage=60'); // Adjust the caching duration as needed (e.g., 60 seconds)
-    
-    res.status(200).json({ tweets });
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).json({ error: "Error fetching data" } as ErrorResponse);
-  }
+  const tweets: Tweet[] = await sanityClient.fetch(feedQuery);
+  revalidatePath('/')
+  console.log(tweets);
+  res.status(200).json({ tweets });
 }
