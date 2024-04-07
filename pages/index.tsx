2fd26aa9c1eadf8bada58be28f5b38dbea578f45
kishan <kishanrokk@gmail.com>
2024-04-07 18:33:34 +0530

    back

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,17 @@ import Head from 'next/head';
 import Sidebar from '@/components/Sidebar';
 import Feed from '@/components/Feed';
 import Widgets from '@/components/Widgets';
-import {
-  GetServerSideProps,
-  InferGetServerSidePropsType,
-  NextPage,
-} from 'next';
+// import type { GetServerSideProps } from "next";
 import { Tweet } from '@/typings';
 import { fetchTweets } from '@/utils/fetchTweets';
 import { Toaster } from 'react-hot-toast';
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
 
 interface Props {
   tweets: Tweet[];
 }
 
-const Home: NextPage<
-  InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ tweets }) => {
+const Home = ({ tweets }: Props) => {
   console.log(tweets);
   return (
     <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
@@ -36,7 +31,7 @@ const Home: NextPage<
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
+export const getServerSideProps: GetServerSideProps = async (_context) => {
   const tweets = await fetchTweets();
   return {
     props: {
@@ -44,47 +39,6 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
     },
   };
 };
-// import Head from "next/head";
-// import Sidebar from "@/components/Sidebar";
-// import Feed from "@/components/Feed";
-// import Widgets from "@/components/Widgets";
-// // import type { GetServerSideProps } from "next";
-// import { Tweet } from "@/typings";
-// import { fetchTweets } from "@/utils/fetchTweets";
-// import { Toaster } from "react-hot-toast";
-// import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
-
-// interface Props {
-//   tweets: Tweet[];
-// }
-
-// const Home = ({ tweets }: Props) => {
-//   console.log(tweets);
-//   return (
-//     <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
-//       <Head>
-//         <title>Twitter</title>
-//       </Head>
-//       <Toaster />
-//       <main className="grid grid-cols-9">
-//         <Sidebar />
-//         <Feed tweets={tweets} />
-//         <Widgets />
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-// export const getServerSideProps: GetServerSideProps = async (_context) => {
-//   const tweets = await fetchTweets();
-//   return {
-//     props: {
-//       tweets,
-//     },
-//   };
-// };
 
 // export const getServerSideProps: GetServerSideProps = async (_context) => {
 //   try {
