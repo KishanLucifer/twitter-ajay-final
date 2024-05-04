c4accf22bfc83c43635456bb77e86cfd0f31620b
kishan <kishanrokk@gmail.com>
2024-05-04 12:49:15 +0530

    random

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -28,11 +28,12 @@ function Feed({ tweets: tweetsProps }: Props) {
     <div className="col-span-7 max-h-screen overflow-scroll divide-y divide-gray-700 scrollbar-hide lg:col-span-5 ">
       <div className="flex items-center justify-between">
         <div>
-          <h1 className="p-5 pb-0 text-xl text-ce font-bold ">For You</h1>
+          <h1 className="p-5 pb-0 text-md text-ce font-bold ">For You</h1>
         </div>
         <div>
-          <h1 className="p-5 pb-0 text-xl text-ce font-bold ">Following</h1>
+          <h1 className="p-5 pb-0 text-md text-ce font-bold ">Following</h1>
         </div>
+
         <RefreshIcon
           onClick={handleRefresh}
           className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
